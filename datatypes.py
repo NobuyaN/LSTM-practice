9ae8507c5686083c94a8d80be2e9ba2bfaa05b03
Nobuya Nishio <nishionob0@gmail.com>
2024-04-12 11:55:42 +0900

    first commit

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 12 11:16:13 2024
+
+@author: nobuya
+"""
+
+a = int(3)
+print(a)
+print(type(a))
+
+a = float(4.2)
+print(a)
+print(type(a))
---
+++ exercise1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 12 11:28:31 2024
+
+Author: Nobuya Nishio
+Student ID: 2600240478-7
+
+Program Description: Practicing variable assignment, type checking, operators, and string methods
+    
+"""
+
+num_1 = 10
+num_2 = 20
+num_3 = 31
+
+average = (num_1 + num_2 + num_3) / 3
+
+print(average)
+print(type(average))
+print(num_1 < num_2)
+
+string_0 = "I dont't know much about programming yet."
+print(string_0)
+print(f"The 4th character is \"{string_0[3]}\"")
+
+my_string = "apples"
+print(len(my_string))
+
+print("Nikola Tesla was a Serbian-American {}, electrical engineer, {}".format("inventor", "mechanical engineer"))
+
+
+
+
